ca052b41fd0acbcdfe7e8910ddb68fe17cf259d7
AD <archied5150@yahoo.com>
2022-06-13 13:24:59 -0500

    created about section

--- photo-port/src/App.js
+++ photo-port/src/App.js
@@ -1,22 +1,19 @@
+import React from 'react';
 import './App.css';
+import About from './components/About'; // import our component
+import Nav from './components/Nav'; // import our component
 
 function App() {
+  /*
+  What happens behind the scenes:
+  React.createElement('div', {}, [ React.createElement('h1',{}, 'ginger'), React.createElement('p', {}, 'breed: Brittany Spaniel') ])
+  */
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Nav></Nav> {/* use our Nav function that was imported */}
+      <main>
+        <About></About> {/* use our About function that was imported */}
+      </main>
     </div>
   );
 }
--- photo-port/src/index.js
+++ photo-port/src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom'; // remove '/client' if using version older than 18.0.0
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+// used with version 18.0.0+
+// const root = ReactDOM.render(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
